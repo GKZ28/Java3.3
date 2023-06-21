bb96a1144ea62276dbbe80f5c97bfef65f65332c
gkz <giorgadzekonstantin@gmail.com>
2023-06-21 16:36:21 +0400

    Added 3 examples

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         int months = 10;
 
         int pay = (int) service.calculate( percents, credit, months);
-        System.out.println("Ежемесячный платеж: " + pay);
+        System.out.println("Ежемесячный платеж: " + (int) service.calculate(9.99, 1_000_000,12));
+        System.out.println();
+        System.out.println("Ежемесячный платеж: " + (int) service.calculate(9.99, 1_000_000,24));
+        System.out.println();
+        System.out.println("Ежемесячный платеж: " + (int) service.calculate(9.99, 1_000_000,36));
+        System.out.println();
     }
 }
